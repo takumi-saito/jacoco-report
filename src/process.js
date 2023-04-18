53a20097bab07f6d905f7c69d68f6c3887d87778
takumi-saito <takumi.saitoh.1992@gmail.com>
2023-04-19 00:50:41 +0900

    process.js ブラッシュアップ

--- src/process.js
+++ src/process.js
@@ -1,102 +1,134 @@
+/**
+ * レポートからファイルのカバレッジを取得
+ * @param {Array} reports - レポートの配列
+ * @param {Array} files - ファイルの配列
+ * @returns {object} ファイルのカバレッジ情報
+ */
 function getFileCoverage(reports, files) {
-  const packages = reports.map((report) => report["package"]);
-  return getFileCoverageFromPackages([].concat(...packages), files);
+  const packages = reports.flatMap((report) => report["package"]);
+  return getFileCoverageFromPackages(packages, files);
 }
 
+/**
+ * パッケージからファイルのカバレッジを取得
+ * @param {Array} packages - パッケージの配列
+ * @param {Array} files - ファイルの配列
+ * @returns {object} ファイルのカバレッジ情報
+ */
 function getFileCoverageFromPackages(packages, files) {
-  const result = {};
-  const resultFiles = [];
-  packages.forEach((item) => {
+  const resultFiles = packages.flatMap((item) => {
     const packageName = item["$"].name;
     const sourceFiles = item.sourcefile;
-    sourceFiles.forEach((sourceFile) => {
+    return sourceFiles.flatMap((sourceFile) => {
       const sourceFileName = sourceFile["$"].name;
-      var file = files.find(function (f) {
-        return f.filePath.endsWith(`${packageName}/${sourceFileName}`);
-      });
-      if (file != null) {
-        const fileName = sourceFile["$"].name;
+      const file = files.find((f) =>
+          f.filePath.endsWith(`${packageName}/${sourceFileName}`)
+      );
+      if (file) {
         const counters = sourceFile["counter"];
-        if (counters != null && counters.length != 0) {
+        if (counters) {
           const coverage = getDetailedCoverage(counters, "INSTRUCTION");
-          file["name"] = fileName;
-          file["missed"] = coverage.missed;
-          file["covered"] = coverage.covered;
-          file["percentage"] = coverage.percentage;
-          resultFiles.push(file);
+          return {
+            ...file,
+            name: sourceFileName,
+            missed: coverage.missed,
+            covered: coverage.covered,
+            percentage: coverage.percentage,
+          };
         }
       }
+      return [];
     });
-    resultFiles.sort((a, b) => b.percentage - a.percentage);
-  });
-  result.files = resultFiles;
-  if (resultFiles.length != 0) {
-    result.percentage = getTotalPercentage(resultFiles);
-  } else {
-    result.percentage = 100;
-  }
-  return result;
+  }).sort((a, b) => b.percentage - a.percentage);
+
+  return {
+    files: resultFiles,
+    percentage: resultFiles.length ? getTotalPercentage(resultFiles) : 100,
+  };
 }
 
+/**
+ * ファイルの総カバレッジを取得
+ * @param {Array} files - ファイルの配列
+ * @returns {number} 総カバレッジ
+ */
 function getTotalPercentage(files) {
-  var missed = 0;
-  var covered = 0;
-  files.forEach((file) => {
-    missed += file.missed;
-    covered += file.covered;
-  });
+  const { covered, missed } = files.reduce(
+      (acc, file) => {
+        acc.missed += file.missed;
+        acc.covered += file.covered;
+        return acc;
+      },
+      { missed: 0, covered: 0 }
+  );
+
   return parseFloat(((covered / (covered + missed)) * 100).toFixed(2));
 }
 
+/**
+ * レポートから全体のカバレッジを取得
+ * @param {Array} reports - レポートの配列
+ * @returns {object} 全体のカバレッジ情報
+ */
 function getOverallCoverage(reports) {
-  const coverage = {};
-  const modules = [];
-  reports.forEach((report) => {
+  const modules = reports.map((report) => {
     const moduleName = report["$"].name;
     const moduleCoverage = getModuleCoverage(report);
-    modules.push({
+    return {
       module: moduleName,
       coverage: moduleCoverage,
-    });
+    };
   });
-  coverage.project = getProjectCoverage(reports);
-  coverage.modules = modules;
-  return coverage;
+
+  return {
+    project: getProjectCoverage(reports),
+    modules: modules,
+  };
 }
 
+/**
+ * レポートからモジュールのカバレッジを取得
+ * @param {object} report - レポートオブジェクト
+ * @returns {number} モジュールのカバレッジ
+ */
 function getModuleCoverage(report) {
   const counters = report["counter"];
   const coverage = getDetailedCoverage(counters, "INSTRUCTION");
   return coverage.percentage;
 }
 
+/**
+ * レポートからプロジェクト全体のカバレッジを取得
+ * @param {Array} reports - レポートの配列
+ * @returns {number} プロジェクト全体のカバレッジ
+ */
 function getProjectCoverage(reports) {
   const coverages = reports.map((report) =>
-    getDetailedCoverage(report["counter"], "INSTRUCTION")
+      getDetailedCoverage(report["counter"], "INSTRUCTION")
   );
   const covered = coverages.reduce(
-    (acc, coverage) => acc + coverage.covered,
-    0
+      (acc, coverage) => acc + coverage.covered,
+      0
   );
   const missed = coverages.reduce((acc, coverage) => acc + coverage.missed, 0);
   return parseFloat(((covered / (covered + missed)) * 100).toFixed(2));
 }
 
+/**
+ * カウンタータイプに応じた詳細なカバレッジ情報を取得
+ * @param {Array} counters - カウンターの配列
+ * @param {string} type - カウンタータイプ
+ * @returns {object} 詳細なカバレッジ情報
+ */
 function getDetailedCoverage(counters, type) {
-  const coverage = {};
-  counters.forEach((counter) => {
-    const attr = counter["$"];
-    if (attr["type"] == type) {
-      const missed = parseFloat(attr["missed"]);
-      const covered = parseFloat(attr["covered"]);
-      coverage.missed = missed;
-      coverage.covered = covered;
-      coverage.percentage = parseFloat(
-        ((covered / (covered + missed)) * 100).toFixed(2)
-      );
-    }
-  });
-  return coverage;
+  const counter = counters.find((c) => c["$"].type === type);
+  const missed = parseFloat(counter["$"].missed);
+  const covered = parseFloat(counter["$"].covered);
+  return {
+    missed: missed,
+    covered: covered,
+    percentage: parseFloat(((covered / (covered + missed)) * 100).toFixed(2)),
+  };
 }
 
 module.exports = {
